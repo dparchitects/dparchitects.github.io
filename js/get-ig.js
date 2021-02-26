8faf259e1566365bcfff645ab5e0756ba5220c0c
leongcv <leongcv@gmail.com>
2021-02-27 02:35:26 +0800

    Updated

--- js/get-ig.js
+++ js/get-ig.js
@@ -1,2 +1,15 @@
-var igToken =
-  "IGQVJXbWttMV84OUw3a25ERm5WbUVzVTV5Q3U0RG54X2dCNW1NYmpKSm16b0c0Tk1CS0thYnR1eE9PX3ZARSFozYV9wQUs1bHVyZAXRPWmFCUHAtSWt6WjNlLXJOeTZAJbmdGcGprSThB";
+var igToken;
+let requestURL = "./superheroes.json";
+let request = new XMLHttpRequest();
+request.open("GET", requestURL);
+request.responseType = "json";
+request.send();
+
+request.onload = function () {
+  const result = request.response;
+  igToken = result["token"];
+};
+console.log("v2!!!!!!!!!!");
+
+// var igToken =
+//   "IGQVJXbWttMV84OUw3a25ERm5WbUVzVTV5Q3U0RG54X2dCNW1NYmpKSm16b0c0Tk1CS0thYnR1eE9PX3ZARSFozYV9wQUs1bHVyZAXRPWmFCUHAtSWt6WjNlLXJOeTZAJbmdGcGprSThB";
